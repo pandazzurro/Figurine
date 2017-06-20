1973a6d693245f8ed3d267b9708d8a3d632ab19b
Andrea Tosato <dossobuono@gmail.com>
2017-06-20 07:54:30 +0200

    Aggiornamento material

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,11 +1,11 @@
 import { NgModule, ModuleWithProviders  } from '@angular/core';
-import { MdListModule, MdSidenavModule, MdToolbarModule, MdButtonModule, MdIconModule,
+import { MdListModule, MdSidenavModule, MdToolbarModule, MdButtonModule, MdIconModule, MdExpansionModule,
     MdButtonToggleModule, MdCardModule, MdChipsModule, MdGridListModule, MdTooltipModule,
     MdIconRegistry
   } from '@angular/material';
 import 'hammerjs';
 
-const MaterialModules = [MdListModule, MdSidenavModule, MdToolbarModule, MdButtonModule,
+const MaterialModules = [MdListModule, MdSidenavModule, MdToolbarModule, MdButtonModule, MdIconModule, MdExpansionModule,
         MdButtonToggleModule, MdCardModule, MdChipsModule, MdGridListModule, MdIconModule, MdTooltipModule];
 
 @NgModule({
